7b4ad92353d76be81e23fad770102931eec5d132
Peter Darche <pdarche@gmail.com>
2016-02-28 22:51:24 -0500

    Add feature expansion code

--- homework2/code/utils.py
+++ homework2/code/utils.py
@@ -1,4 +1,5 @@
 from __future__ import division
+import itertools
 
 
 def compute_errors(predictions, labels):
@@ -17,3 +18,16 @@ def test_scikits_model(clf, test_data, test_labels):
     errors = compute_errors(preds, [t[0] for t in test_labels])
 
     return len(errors) / test_labels.shape[0]
+
+
+def combinations(x):
+    combos = np.array(list(itertools.combinations(x,2)))
+    x_prime = np.array([x1 * x2.T for x1, x2 in combos])
+
+    return x_prime
+
+
+def expand_features(original_features):
+    squared = original_features ** 2
+    combos = combinations(features)
+    return np.concatenate([original_features, squared, combos])
