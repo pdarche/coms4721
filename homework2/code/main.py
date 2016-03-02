2ba63d8b9b49e1a3f24bc87148d16a7a84f29a0f
Peter Darche <pdarche@gmail.com>
2016-03-02 00:18:33 -0500

    clean and package code

---
+++ homework2/code/main.py
@@ -0,0 +1,42 @@
+"""
+Main Driver Script for the Assignment
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Classfiers other than AveragedPerceptron are from scikits-learn
+LinearDiscriminantAnalysis      : http://scikit-learn.org/stable/modules/generated/sklearn.discriminant_analysis.LinearDiscriminantAnalysis.html
+QuadraticDiscriminantAnalysis   : http://scikit-learn.org/stable/modules/generated/sklearn.discriminant_analysis.QuadraticDiscriminantAnalysis.html
+LogisticRegression              : http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
+
+Code for the AveragedPerceptron can be found in avgperceptron.py
+Code for cross-validation and other methods is in utils.py
+"""
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from avgperceptron import AveragedPerceptron
+
+from utils import *
+
+if __name__ == '__main__':
+    models = [
+        ('Averaged Perceptron', AveragedPerceptron),
+        ('Logistic Regression', LogisticRegression),
+        ('QDA', QuadraticDiscriminantAnalysis),
+        ('LDA', LinearDiscriminantAnalysis),
+        ('Averaged Perceptron Expanded', AveragedPerceptron),
+        ('Logistic Regression Expanded', LogisticRegression)
+    ]
+
+    scored_models = list(score_models(models))
+
+    # 1. Cross-validation error rates for all methods
+    cross_validation_error_rates(scored_models)
+
+    # 2. Training error rate of the classifier learned by the selected method (and state which method was chosen)
+    best_classifier_training_error_rate(scored_models)
+
+    # 3. Test error rate for the learned classifier
+    best_classfier_test_error_rate(scored_models)
+
--- homework2/code/utils.py
+++ homework2/code/utils.py
@@ -4,9 +4,6 @@ import itertools
 
 
 def compute_errors(predictions, labels):
-    """ Generates a list of indexes of misclassified
-    examples
-    """
     zipped = zip(predictions, labels)
     errors = [ix for ix, tup in enumerate(zipped)
               if tup[0] != tup[1]]
@@ -55,13 +52,49 @@ def k_fold_cross_validation(data, labels, num_folds):
 def score_models(models):
     for name, Model in models:
         errors = []
+        classifiers = []
         num_folds = 10
         for training, testing in k_fold_cross_validation(spam_data, spam_labels, num_folds):
+            if 'Expanded' in name:
+                training['data'] = np.array([expand_features(x) for x in training['data']])
+                testing['data'] = np.array([expand_features(x) for x in testing['data']])
             clf = Model()
             clf.fit(training['data'], training['labels'])
             error = test_scikits_model(clf, testing['data'], testing['labels'])
             errors.append(error)
+            classifiers.append(clf)
         avg_error = np.sum(errors) / num_folds
 
-        yield (name, avg_error, errors)
+        yield (name, avg_error, errors, classifiers)
+
+
+def select_classifer(scored_models):
+    errors = np.concatenate([s[2] for s in scored_models])
+    classifiers = np.concatenate([s[3] for s in scored_models])
+    min_error_ix = np.argmin(errors)
+    name_ix = min_error_ix//10
+    clf_name = [m[0] for m in scored_models][name_ix]
+    return (clf_name, errors[min_error_ix], classifiers[min_error_ix])
+
+
+def cross_validation_error_rates(scored_models):
+    for scored in scored_models:
+        print "{} Cross Validation Error Rate: {}".format(scored[0], scored[1])
+
+
+def best_classifier_training_error_rate(scored_models):
+    clf_name, clf_error, clf = select_classifer(scored_models)
+
+    print "%s Training Error Rate: %.4f" % (clf_name, clf_error)
+
+
+def best_classfier_test_error_rate(scored_models, test_data, test_labels):
+    clf_name, clf_error, clf = select_classifer(scored_models)
+    if 'Expanded' in clf_name:
+        test_data = np.array([expand_features(x) for x in test_data])
+    test_error = test_scikits_model(clf, test_data, test_labels)
+
+    print "%s Testing Error Rate %.4f" % (clf_name, clf_error)
+
+
 
